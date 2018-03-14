da2d8dc4ef7f13a79e2fa2ceb8a4d70b2ea2e5a6
Yuri Grinshteyn <ygrinshteyn@yuri02.c.googlers.com>
2018-03-14 19:19:21 -0400

    adding SD metric creation

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
 var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
+// Imports the Google Cloud client library
+const monitoring   =         require('@google-cloud/monitoring');
+// Creates a client
+const client       =         new monitoring.MetricServiceClient();
+//specify GCP Project ID
+const projectId = 'ygrinshteyn-sandbox';
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// ----------- create custom metric for Stackdriver ------------
+//specify information for custom metric
+
+const request = {
+  name: client.projectPath(projectId),
+  metricDescriptor: {
+    description: 'Manually entered numeric value',
+    displayName: 'Number Entered',
+    type: 'custom.googleapis.com/global/numeric_netric',
+    metricKind: 'GAUGE',
+    valueType: 'DOUBLE',
+    unit: '{Value}',
+  },
+};
+
+// Creates a custom metric descriptor
+client
+  .createMetricDescriptor(request)
+  .then(results => {
+    const descriptor = results[0];
+
+    console.log('Created custom Metric:\n');
+    console.log(`Name: ${descriptor.displayName}`);
+    console.log(`Description: ${descriptor.description}`);
+    console.log(`Type: ${descriptor.type}`);
+    console.log(`Kind: ${descriptor.metricKind}`);
+    console.log(`Value Type: ${descriptor.valueType}`);
+    console.log(`Unit: ${descriptor.unit}`);
+    console.log('Labels:');
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
 
 //display form on get request
 app.get('/',function(req,res){
