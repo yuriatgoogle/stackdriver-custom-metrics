d85b2d1aefe69004286ab8d069852720c5e68b9e
Yuri Grinshteyn <ygrinshteyn@yuri02.c.googlers.com>
2018-03-14 18:35:08 -0400

    updating comments

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ var app            =         express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//display form on get request
 app.get('/',function(req,res){
   res.sendfile("index.html");
 });
+
+// handle form submit
 app.post('/',function(req,res){
+  //get value
   var metric_value=req.body.metric_value;
+  //output value
   console.log("Metric value = "+metric_value);
   res.end("yes");
 });
+
+//start server
 app.listen(8080,function(){
   console.log("Started on PORT 8080");
 })
