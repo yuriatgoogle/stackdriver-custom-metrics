3f39118a3c4df15fb0e7a62b6537f42833c5a1e8
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-04-03 17:11:37 -0700

    Continuing work on flask framework

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
 import logging
 from app.forms import PostForm
 logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    logging.info('Index hit!')
     form=PostForm()
-    return render_template('index_python.html', form=form)
+    if form.validate_on_submit():
+        flash('value entered {}'.format(
+            form.metric.data))
+        logging.warning('value entered is ' + form.metric.data) # log the input
+        # TODO - call a function to send the metric to Stackdriver
+        return redirect('/index') # redirect back to main page
+    return render_template('index_python.html', form=form) # show the form page
+
+
+
+    
+    
