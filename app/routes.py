491161922febb3504622c9efa224468bf11401f7
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-04-03 17:51:32 -0700

    metric writing works, need to fix resource

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, flash
 import logging
+from google.cloud import monitoring
 from app.forms import PostForm
 logging.basicConfig(level=logging.DEBUG)
 
@@ -8,11 +9,28 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form=PostForm()
+    client = monitoring.Client()
     if form.validate_on_submit():
         flash('value entered {}'.format(
             form.metric.data))
-        logging.warning('value entered is ' + form.metric.data) # log the input
+        logging.warning('value entered is ' + (form.metric.data)) # log the input
         # TODO - call a function to send the metric to Stackdriver
+        # ----- for now - doing it here
+        metric = client.metric(
+            type_='custom.googleapis.com/numeric_value',
+            labels={}
+        )
+        resource = client.resource( # resource is not being written properly
+            type_='global',
+            labels={
+            }
+        )
+        # Default arguments use endtime datetime.utcnow()
+        client.write_point(metric, resource, int(form.metric.data))
+        logging.info('Successfully wrote time series.')
+
+
+        # ------- end metric write
         return redirect('/index') # redirect back to main page
     return render_template('index_python.html', form=form) # show the form page
 
